d80615fe44881d432b9e576b26f62df07f187437
keerthivelp <149885784+keerthivelp@users.noreply.github.com>
2025-04-03 10:51:29 +0530

    Time: 1 ms (99.9%), Space: 44.3 MB (98.42%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (ind >= arr.length) { 
+            if (target == 0) {
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds); 
+            ds.remove(ds.size() - 1);
+        }
+
+       
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] candidates1 = {2, 3, 6, 7};
+        int target1 = 7;
+        System.out.println(sol.combinationSum(candidates1, target1));
+
+        int[] candidates2 = {2, 3, 5};
+        int target2 = 8;
+        System.out.println(sol.combinationSum(candidates2, target2));
+    }
+}
